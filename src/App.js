00229bd11b16d7d60b6f6e3ce67a77f2e891b90c
Kal <58021911+kalyano@users.noreply.github.com>
2020-04-14 01:02:54 -0400

    Update App.js
    edited text to test pull request > merge > push travis CI deploy to AWS

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          K3 was not here!
+          K3 was back via pull request!
         </p>
         <a
           className="App-link"
